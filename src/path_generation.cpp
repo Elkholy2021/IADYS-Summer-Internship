0d474c76a1ce6f5d71a62b818bb351600e71c06d
jaiem <lotfi.jaiem@iadys.com>
2022-08-19 15:41:25 +0200

    Jour 35 v2

--- src/path_generation.cpp
+++ src/path_generation.cpp
@@ -7,15 +7,68 @@
 #include<string>
 
 using namespace std;
-#include "ros/ros.h"
-
 #include <iomanip>
 #include <vector>
 #include <utility>
 
-class control_jellyfishbot {        
+class path_generation {        
   public:
-      double m= 18; //kg
-      double Iz= 2.37; //inertia around z
-      double dt=0.1; //time interval in seconds, depending on the frequency of the sensor readings
+      double W , L , R , x0 , y0 , alpha;
+      int points_per_side; 
+      double x , y;
+      bool other_side;
+      
+  void init_path(){
+      points_per_side = floor(W/R);
+      
+  }
+  void generate_points(){
+      
+      double points[2][floor(W/R)*2+1];
+
+      points[0][0] = 0;
+      points[1][0] = 0;
+      x = x0;
+      y = y0;
+      other_side = true;
+      for (int i = 0; i <= points_per_side; ++i) {
+        //cout << i << " ";
+        points[0][i+1] = x;
+        points[1][i+1] = y;
+        if (other_side == true){
+          if (x == x0 + L){
+            x = x - L;
+            y = y;
+            other_side = false;
+          }
+          else{
+            x = x + L;
+            y = y;
+            other_side = false;
+          }
+        }
+        else{
+          x = x;
+          y = y + R;
+          other_side = true;
+        }
+
+
+
+    }
+
+  }
+};
+int main(){
+  path_generation path_generation;
+  path_generation.W = 20;
+  path_generation.L = 8;
+  path_generation.R = 3;
+  path_generation.x0 = 2;
+  path_generation.y0 = 2;
+  path_generation.alpha = 0;
+  path_generation.init_path();
+  path_generation.generate_points();
+  for (int i = 0; i <= floor(path_generation.W/path_generation.R); ++i) {
+    cout << path_generation.points[0][i+1] << "," << path_generation.points[1][i+1]<< endl;;
 }
